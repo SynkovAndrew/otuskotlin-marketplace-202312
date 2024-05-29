6c77d6e61b66a6f1d4539de401b289193488062d
asynkov <asynkov@belkacar.ru>
2024-05-29 22:08:49 +0300

    in progress

--- project/stocktrack-backend/stocktrack-business/src/main/kotlin/com/otus/otuskotlin/stocktrack/dsl/stub/StubForSucceededCreateCommand.kt
+++ project/stocktrack-backend/stocktrack-business/src/main/kotlin/com/otus/otuskotlin/stocktrack/dsl/stub/StubForSucceededCreateCommand.kt
@@ -7,6 +7,10 @@ import com.otus.otuskotlin.stocktrack.cor.ChainDsl
 import com.otus.otuskotlin.stocktrack.model.Command
 import com.otus.otuskotlin.stocktrack.model.Debug
 import com.otus.otuskotlin.stocktrack.model.State
+import com.otus.otuskotlin.stocktrack.stock.ErrorStockRepositoryResponse
+import com.otus.otuskotlin.stocktrack.stock.OkStockRepositoryResponse
+import com.otus.otuskotlin.stocktrack.stock.OkWithErrorsStockRepositoryResponse
+import com.otus.otuskotlin.stocktrack.stock.StockRepositoryRequest
 
 fun ChainDsl<SingleStockResponseContext>.stubForSucceededCreateCommand(
     coreSettings: CoreSettings
@@ -24,7 +28,10 @@ fun ChainDsl<SingleStockResponseContext>.stubForSucceededCreateCommand(
 
         process {
             logger.callLogged(it.requestId.asString(), LogLevel.DEBUG) {
-                it.copy(state = State.FINISHED, response = it.request)
+                StockRepositoryRequest(stock = it.request)
+                    .let { request -> coreSettings.stubStockRepository.create(request) }
+                    .let { response -> it.copy(response = (response as OkStockRepositoryResponse).data).finish() }
+                        as SingleStockResponseContext
             }
         }
     }
